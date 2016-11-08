d7731ffde9f3f5b8deef4e023580dd8344008cef
MidnightP <heijden.pvd@gmail.com>
2016-11-08 20:02:09 +0100

    added game top-level store and action files

--- client/reducers/game.js
+++ client/reducers/game.js
@@ -1,14 +1,28 @@
 
-// going downstream
-import UPDATE_PLAYER from '../actions/update-player'
+// going upstream
+import UPDATE_PLAYER from '../actions/update-players'
 import UPDATE_GAME  from '../actions/update-game'
 import UPDATE_LEVEL from '../actions/update-level'
+import UPDATE_SWORDS from '../actions/update-swords'
 
-
-// going upstream
-import UPDATE_PLAYERS from '../actions/update-players'
+// going downstream
+import UPDATE_PLAYERS from '../actions/update-player'
 // fetch all players optional ?
 
 //  fetch all sowrds optional ?
 
-export default
+export default (state = {}, { type, payload } = {} ) => {
+
+  switch(type) {
+    case UPDATE_PLAYER:
+
+    case UPDATE_GAME:
+
+    case UPDATE_LEVEL:
+
+    case UPDATE_PLAYERS:
+
+    default :
+      return state
+  }
+}
