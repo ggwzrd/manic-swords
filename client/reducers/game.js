af44d2f36368cda6ed7ad9acb2d9100df8b94634
giuliogallerini <giulio.gallerini.work.com>
2016-11-08 21:28:07 +0100

    implement update_game action fixed small bug game reducer add dark style

--- client/reducers/game.js
+++ client/reducers/game.js
@@ -9,18 +9,22 @@ import UPDATE_SWORDS from '../actions/update-swords'
 import UPDATE_PLAYERS from '../actions/update-player'
 // fetch all players optional ?
 
-//  fetch all sowrds optional ?
+//  fetch all swords optional ?
 
 export default (state = {}, { type, payload } = {} ) => {
 
   switch(type) {
     case UPDATE_PLAYER:
-
+      return payload
+      
     case UPDATE_GAME:
+      return payload
 
     case UPDATE_LEVEL:
+      return payload
 
     case UPDATE_PLAYERS:
+      return payload
 
     default :
       return state
