4d4654b2a22bf822999ba78e94af52782b21a627
MidnightP <heijden.pvd@gmail.com>
2016-11-09 21:16:50 +0100

    isPlayer is not defined

--- client/components/Canvas.js
+++ client/components/Canvas.js
@@ -1,31 +1,40 @@
-import React, { Component } from 'react'
+import React, { Component, PropTypes } from 'react'
 import { connect } from 'react-redux'
 import saveGame from '../actions/update-game'
 import './Canvas.sass'
 
 
 class Canvas extends React.Component {
-    componentDidMount() {
+
+  componentDidMount() {
         this.updateCanvas();
+
+
+        window.addEventListener( 'keydown', function(event) {
+            this.updatePlayer(event).bind(this)
+        }.bind(this))
+
     }
 
     updateCanvas() {
-       
-        console.log(currentUser.position.x)
+
+        // console.log(currentUser.position.x)
 
         // call functions:
         // 1) player hit by sword?  YES -> update-player
         // 2) key down?         YES -> update-player
 
+        const ctx = this.refs.canvas.getContext('2d');
+        ctx.fillRect(300,300,300,300);
     }
 
     updatePlayer(event) {
 
-      // const ctx = this.refs.canvas.getContext('2d');
-      // ctx.fillRect(300,300,300,300);
+      const { currentUser, game, isPlayer } = this.props
+      console.log(isPlayer)
+      if( !isPLayer ) return
 
       console.log(event)
-      console.log(currentUser.position.x)
     }
 
     // check if player is hitted by sword
@@ -33,15 +42,16 @@ class Canvas extends React.Component {
 
     render() {
         return (
-            <canvas ref="canvas" width={1200} height={600} onKeyDown = { this.updatePlayer.bind(this) } />
+            <canvas ref="canvas" width={1200} height={600} />
         );
     }
 }
 
 // copied this from Game.js
 Canvas.propTypes = {
-  game: PropTypes.object.isRequired,
+  gameStatus: PropTypes.object.isRequired,
   currentUser: PropTypes.object.isRequired,
+  isPlayer: PropTypes.bool.isRequired
 }
 
 export default connect()(Canvas)
