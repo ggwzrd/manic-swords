510053ca6b054f301e68ba9989d92181ae3b9450
giuliogallerini <giulio.gallerini.work.com>
2016-11-09 20:11:09 +0100

    connect canvas

--- client/components/Canvas.js
+++ client/components/Canvas.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import saveGame from '../actions/update-game'
 import './Canvas.sass'
 
 
@@ -14,7 +16,7 @@ class Canvas extends React.Component {
         // call functions:
         // 1) player hit by sword?  YES -> update-player
         // 2) key down?         YES -> update-player
-        
+
     }
 
     // check if player is hitted by sword
