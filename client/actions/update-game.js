ef5e47f81c6d5521f2839ebeaaf3914bcd819bab
MidnightP <heijden.pvd@gmail.com>
2016-11-10 15:16:33 +0100

    puppets are fixed to player created helper for update-player

--- client/actions/update-game.js
+++ client/actions/update-game.js
@@ -2,27 +2,27 @@ import appLoading from './loading'
 import gameModel from '../models/GameModel'
 import signOutUser from './destroy-session-user'
 
-// export const GAME_UPDATED = 'GAME_UPDATED'
+export const GAME_UPDATED = 'GAME_UPDATED'
 
 export default (game, properties = {}, reset = false) => {
   return (dispatch) => {
-    dispatch(appLoading(true))
+    // dispatch(appLoading(true))
 
     gameModel.dispatch = dispatch
     gameModel.app.authenticate()
       .then((response) => {
         gameModel.save(game, properties, reset)
-        dispatch(appLoading(false))
+        // dispatch(appLoading(false))
       }).catch((error) => {
-        dispatch(appLoading(false))
+        // dispatch(appLoading(false))
         console.error(error)
         dispatch(signOutUser())
       })
   }
 }
 
-// export function gameUpdate() {
-//   return {
-//     type: GAME_UPDATED
-//   }
-// }
+export function gameUpdate() {
+  return {
+    type: GAME_UPDATED
+  }
+}
---
+++ client/helpers/update-player-helper.js
@@ -0,0 +1,60 @@
+const currentPlayer = (_self) => {
+    // if the currentUser has created or joined the game this should return a player
+    const { game, currentUser } = _self.props
+    return game.players.filter((player) =>
+    player.userId === currentUser._id)[0]
+
+}
+const otherPlayer = (_self) => {
+  // if another player has joined the game AND there is a currentPlayer,
+  // this function will return the other player
+  // if we wanna have all the others, get them here as an an array
+  const { game, currentUser } = _self.props
+  return game.players.filter((player) =>
+  player.userId !== currentUser._id)[0]
+}
+
+export const updatePlayer = (_self, event) => {
+
+    const { saveGame, game } = _self.props
+    const { players } = game.players
+
+    const player1 = currentPlayer(_self)
+    const player2 = otherPlayer(_self)
+
+    if(!!!player1) return false
+
+    const positionX = player1.position.x
+    const positionY = player1.position.y
+
+    const moveAmount = 18
+
+    switch(event.keyCode){
+      case 37:  //left
+        const newPlayersLeft = [player2, Object.assign({}, player1,
+          {position: {x: positionX - moveAmount, y: positionY}
+        })]
+        saveGame(game, { players: newPlayersLeft })
+        return false
+      case 39:  // right
+        const newPlayersRight = [player2, Object.assign({}, player1,
+          {position: {x: positionX + moveAmount, y: positionY}
+        })]
+        saveGame(game, { players: newPlayersRight })
+        return
+      case 38:  // up
+        const newPlayersUp = [player2, Object.assign({}, player1,
+          {position: {x: positionX, y: positionY - moveAmount}
+        })]
+        saveGame(game, { players: newPlayersUp })
+        return
+      case 40:  // down
+        const newPlayersDown = [player2, Object.assign({}, player1,
+          {position: {x: positionX, y: positionY + moveAmount}
+        })]
+        saveGame(game, { players: newPlayersDown })
+      return
+      default :
+        return
+    }
+}
