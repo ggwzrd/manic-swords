a6484bf637e6d7d9ac95a7d30820b933684a0684
MidnightP <heijden.pvd@gmail.com>
2016-11-11 11:00:02 +0100

    some comments

--- client/components/Canvas.js
+++ client/components/Canvas.js
@@ -17,27 +17,30 @@ let clientSwords = []
 
 class Canvas extends React.Component {
 
-  componentWillMount(){
-    const { swords, players, levels } = this.props.game
-    clientSwords = swords.map((sword) => {
-      return new Sword(sword, levels[0])
-    })
-  }
+    componentWillMount(){
+        const { swords, players, levels } = this.props.game
+        clientSwords = swords.map((sword) => {
+          return new Sword(sword, levels[0])
+        })
+    }
+
+    // we bind addEventListener to update the player position
+    // when the component has mounted
     componentDidMount() {
+       // we draw the players for the first time
       this.drawPlayers()
       console.log('Hey, we`re MOUNTING')
       window.addEventListener( 'keydown', function(event) {
         updatePlayer(this, event)
       }.bind(this))
 
+      // here we trigger the draw function for the first time
+      // we should call this at least once to start the loop
+      // to continuously trigger the drawPlayer and drawSwords functions
       this.draw()
     }
 
-    // componentDidUpdate() {
-    //     console.log('Hey, I received props!')
-    //     this.updateCanvas()
-    // }
-
+    // we continuously draw all swords and players
     draw(){
         const { swords } = this.props.game
         const ctx = this.refs.canvas.getContext('2d')
@@ -53,17 +56,22 @@ class Canvas extends React.Component {
         window.requestAnimationFrame(this.draw.bind(this))
     }
 
+    // this is how we draw swords
     drawSwords(swords) {
 
       const ctx = this.refs.canvas.getContext('2d')
       const swordImg = new Image()
       clientSwords.map((sword) => {
+
+        // the falling class function increments the y-coordinates
+        // of the swords each time we draw
         sword.falling()
         swordImg.src = sword.image
         ctx.drawImage(swordImg, sword.position.x, sword.position.y)
       })
     }
 
+    // this is how we draw players
     drawPlayers() {
         const { players } = this.props.game
         const ctx = this.refs.canvas.getContext('2d')
@@ -86,11 +94,13 @@ class Canvas extends React.Component {
     }
 }
 
-// copied this from Game.js
+// copied this from Game.js. This sort of validates the game and currentUser
 Canvas.propTypes = {
   game: PropTypes.object.isRequired,
   currentUser: PropTypes.object.isRequired,
 }
 
-// we don't use mapStateToProps here cause we got the props from the Game
+// we don't use mapStateToProps here
+// because we feed the needed props straight
+// to the Canvas tag in the Game component
 export default connect(null, { saveGame })(Canvas)
